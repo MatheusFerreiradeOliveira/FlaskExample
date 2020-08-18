ac6ae0a880b6b09cb7b7f3206640fe838cad9955
matheusferreiradeoliveira <matheusferreiradeoliveira21@gmail.com>
2020-08-17 22:11:19 -0300

    new routes

--- Api/app.py
+++ Api/app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello world'
 
+@app.route('/author')
+def my_name():
+    return 'Matheus Oliveira'
+
+@app.route('/age')
+def my_age():
+    return '21'
+
 if __name__ == '__main__':
     app.run()
